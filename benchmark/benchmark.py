625af3278a7e93a6685b4ce50a38b15dc2af02d9
masterSunflowers <lvdthieu@gmail.com>
2025-05-12 00:10:06 +0700

    update: update benchmark.py to test new experiment

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -5,7 +5,7 @@ from eval import cal_edit_sim, cal_exactly_match
 def main(args):
     df = pd.read_json(args.input, lines=True)
     references = df["groundtruth"].tolist()
-    preds = df["completion_refine"].fillna("").tolist()   # preds = df["completions"].fillna("").tolist()
+    preds = df["predict"].fillna("").tolist()   # preds = df["completions"].fillna("").tolist()
     em = cal_exactly_match(references, preds)
     edit_sim = cal_edit_sim(references, preds)
     print("==========RESULT==========")
@@ -20,3 +20,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input", type=str, dest="input")
     args = parser.parse_args()
     main(args)
+
+
+# 19.15 62.81
+# 27.66 75.17
